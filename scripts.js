2fbeef433a3a5bb29f215f5e4f6877a8c5e242b2
Andrew L Thomas <68823420+DollarStoreThor@users.noreply.github.com>
2024-11-22 19:03:26 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,15 +8,27 @@ document.addEventListener("DOMContentLoaded", () => {
   let images = [];
 
   // Function to fetch random images
-  function fetchImages(folder) {
-    // Simulate fetching images from a folder (e.g., replace this array with an API call)
-    return [
-      `${folder}/image1.jpg`,
-      `${folder}/image2.jpg`,
-      `${folder}/image3.jpg`,
-      `${folder}/image4.jpg`,
-    ];
-  }
+function fetchImages(folder) {
+  const imageMap = {
+    "turnover-images": [
+      "turnover-images/13%20Best%20Predictors%20of%20Employee%20Turnover%20from%20LassoCV%20Model%20-%20How%20these%20features%20Correlate%20with%20Eachother.png",
+      "turnover-images/Clusters%20of%20Employee%20Turnover.png",
+      "turnover-images/Confusion%20Matrix%20for%20GBC.png",
+    ],
+    "bookshelf-images": [
+      "bookshelf-images/book1.jpg",
+      "bookshelf-images/book2.jpg",
+      "bookshelf-images/classifier-demo.png",
+    ],
+    "realestate-images": [
+      "realestate-images/map-analysis.jpg",
+      "realestate-images/clustering.png",
+      "realestate-images/prediction-results.png",
+    ],
+  };
+
+  return imageMap[folder] || [];
+}
 
   // Function to show the slideshow modal
   function showSlideshow(folder) {
